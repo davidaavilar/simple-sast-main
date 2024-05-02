d654095d4fc96b395a28e9ba3e25976f9fe45951
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-05-02 11:58:17 -0500

    added files

---
+++ index.js
@@ -0,0 +1,5 @@
+import {template} from "lod";
+template();
+
+import { trim as hopa} from "axios";
+hopa();
---
+++ new.js
@@ -0,0 +1,9 @@
+import { trim as hello} from "mpath";
+// import {template} from "lodash";
+// template();
+import { trim as hopa} from "axios";
+import {setKey} from "minimist";
+setKey();
+
+hopa();
+hello();
